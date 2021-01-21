4cf18685ea13d8d4a46206cc5893e0e570d191f6
Rafal Jedel <noahh.r@gmail.com>
2021-01-21 02:47:22 +0100

    adding a location by address

--- car_park/forms.py
+++ car_park/forms.py
@@ -5,19 +5,44 @@ from django.contrib.auth.models import User
 
 class AddCarParkForm(forms.Form):
     name = forms.CharField(label='Nazwa', min_length=2, max_length=100)
-    description = forms.CharField(label='Opis', min_length=2, required=False,
+    description = forms.CharField(label='Opis', required=False,
                                   widget=forms.Textarea(attrs={'cols': 21, 'placeholder': 'pole nie jest wymagane'}))
     spot_name = forms.CharField(label='Miejscowość', min_length=2, max_length=100)
     street = forms.CharField(label='Ulica', min_length=2, max_length=100, required=False)
     street.widget.attrs.update(placeholder='pole nie jest wymagane')
-    street_number = forms.IntegerField(label='Numer', min_value=0)
-    free_of_charge = forms.BooleanField(label='Czy jest bezpłatny')
+    street_number = forms.IntegerField(label='Numer', widget=forms.TextInput)
+    longitude = forms.CharField(widget=forms.HiddenInput())
+    latitude = forms.CharField(widget=forms.HiddenInput())
+    free_of_charge = forms.BooleanField(label='Czy jest bezpłatny', required=False)
+    tariffs_name = forms.CharField(label='Nazwa taryfy', required=False,
+                                   widget=forms.TextInput(attrs={'placeholder': 'pole nie jest wymagane'}))
     first_hour_fee = forms.DecimalField(label='Opłata za pierwszą godzinę postoju', max_digits=5, decimal_places=2,
                                         min_value=0, required=False)
     maximum_additional_fee = forms.DecimalField(label='Maksymalna opłata dodatkowa', max_digits=5, decimal_places=2,
                                                 min_value=0, required=False)
-    additional_fee_description = forms.CharField(label='Opis maksymalnej dopłaty', min_length=2, required=False,
-                                                 widget=forms.Textarea)
+    additional_fee_description = forms.CharField(label='Opis maksymalnej dopłaty', required=False,
+                                                 widget=forms.Textarea(attrs={'cols': 21}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        free_of_charge = cleaned_data.get('free_of_charge')
+        first_hour_fee = cleaned_data.get('first_hour_fee')
+        maximum_additional_fee = cleaned_data.get('maximum_additional_fee')
+        additional_fee_description = cleaned_data.get('additional_fee_description')
+        longitude = cleaned_data.get('longitude')
+        latitude = cleaned_data.get('latitude')
+        if free_of_charge and (first_hour_fee or maximum_additional_fee or additional_fee_description):
+            raise forms.ValidationError(
+                'Nie zaznaczaj pola "Czy jest bezpłatny" '
+                'razem z wypełnionym polem dotyczącym opłat'
+            )
+        if not free_of_charge and not any([first_hour_fee, maximum_additional_fee, additional_fee_description]):
+            raise forms.ValidationError(
+                'Proszę wypełnij przynajmniej jedno z trzech pól dotyczących opłaty'
+            )
+        if not longitude and not latitude:
+            raise forms.ValidationError('Nie można dodać parkingu bez jego lokalizacji')
+        return cleaned_data
 
 
 class CustomUserCreationForm(UserCreationForm):
--- car_park/views.py
+++ car_park/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render
+from django.contrib.gis.geos import fromstr
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import DetailView, ListView, CreateView
 
 from .forms import CustomUserCreationForm, AddCarParkForm
-from .models import CarPark
+from .models import CarPark, Tariff
 
 
 class SignupView(CreateView):
@@ -33,6 +35,47 @@ class CarParkDetailView(DetailView):
 class AddCarParkView(View):
     def get(self, request):
         return render(request, 'car_park/add_car_park.html', {'form': AddCarParkForm()})
+
+    def post(self, request):
+        form = AddCarParkForm(data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            longitude = form.cleaned_data['longitude']
+            latitude = form.cleaned_data['latitude']
+            # longitude = "20.892127886317226"
+            # latitude = "52.232701199999994"
+
+            free_of_charge = form.cleaned_data['free_of_charge']
+            tariffs_name = form.cleaned_data['tariffs_name']
+            first_hour_fee = form.cleaned_data['first_hour_fee']
+            maximum_additional_fee = form.cleaned_data['maximum_additional_fee']
+            additional_fee_description = form.cleaned_data['additional_fee_description']
+
+            tariff = None
+            if not free_of_charge:
+                tariff = Tariff.objects.create(
+                    tariffs_name=tariffs_name,
+                    first_hour_fee=first_hour_fee,
+                    maximum_additional_fee=maximum_additional_fee,
+                    additional_fee_description=additional_fee_description,
+                )
+            car_park = CarPark.objects.create(
+                name=name,
+                description=description,
+                location=fromstr('POINT({} {})'.format(longitude, latitude), srid=4326),
+                free_of_charge=free_of_charge,
+                tariff=tariff,
+            )
+            return redirect(reverse('car_park_detail', args=[car_park.pk]))
+        return render(request, 'car_park/add_car_park.html', {'form': AddCarParkForm(
+            data=request.POST,
+            initial={
+                'spot_name': '',
+                'street': '',
+                'street_number': None,
+            }
+        )})
 
 
 class AboutView(View):
